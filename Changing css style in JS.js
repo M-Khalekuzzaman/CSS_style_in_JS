b8106ed515acc3350762d0088a42e026d5cb9e0f
Kaochar <94604055+M-Khalekuzzaman@users.noreply.github.com>
2021-11-29 09:54:46 +0600

    Add files via upload

---
+++ Changing css style in JS.js
@@ -0,0 +1,16 @@
+
+
+
+function addStyle()
+{
+   var myVar = document.querySelector("#paraId");
+    myVar.classList.add("my-style");
+
+}
+
+function removeStyle()
+{
+   var myVar = document.querySelector("#paraId");
+    myVar.classList.remove("my-style");
+
+}
